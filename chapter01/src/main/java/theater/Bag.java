1b1cc8872e72a5e077954bd3fde875f97200d377
YONGJU <odetsuo@gmail.com>
2022-05-11 08:18:00 +0900

    feat: 초대장, 티켓, 가방

---
+++ chapter01/src/main/java/theater/Bag.java
@@ -0,0 +1,29 @@
+package theater;
+
+public class Bag {
+
+    private Long amount;
+    private Invitation invitation;
+    private Ticket ticket;
+
+    public boolean hasInvitation() {
+        return invitation != null;
+    }
+
+    public boolean hasTicket() {
+        return ticket != null;
+    }
+
+    public void setTickets(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+
+}
---
+++ chapter01/src/main/java/theater/Invitation.java
@@ -0,0 +1,9 @@
+package theater;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+
+    private LocalDateTime when;
+
+}
