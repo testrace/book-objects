bf5b1debfa98c58a9c11c1c8d42d2945bdf950cb
YONGJU <odetsuo@gmail.com>
2022-05-11 08:20:24 +0900

    feat: 가방 생성자 추가
    공연 관람객의 유형(이벤트 당첨자, 미당첨자)에 따라 소지품의 유무에 대한 제약

--- chapter01/src/main/java/theater/Bag.java
+++ chapter01/src/main/java/theater/Bag.java
@@ -6,6 +6,15 @@ public class Bag {
     private Invitation invitation;
     private Ticket ticket;
 
+    public Bag(Long amount) {
+        this.amount = amount;
+    }
+
+    public Bag(Long amount, Invitation invitation) {
+        this.amount = amount;
+        this.invitation = invitation;
+    }
+
     public boolean hasInvitation() {
         return invitation != null;
     }
