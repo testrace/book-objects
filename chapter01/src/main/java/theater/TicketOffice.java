95306737c84eddc0820d860601f877cd695f3974
YONGJU <odetsuo@gmail.com>
2022-05-11 08:26:23 +0900

    feat: 매표소
    판매할 티켓들을 가진다.
    티켓을 판매한 금액을 가진다.

---
+++ chapter01/src/main/java/theater/TicketOffice.java
@@ -0,0 +1,27 @@
+package theater;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketOffice {
+
+    private Long amount;
+    private List<Ticket> tickets = new ArrayList<>();
+
+    public TicketOffice(Long amount, Ticket... tickets) {
+        this.amount = amount;
+        this.tickets.addAll(List.of(tickets));
+    }
+
+    public Ticket getTicket() {
+        return tickets.remove(0);
+    }
+
+    public void minusAmount(Long amount) {
+        this.amount -= amount;
+    }
+
+    public void plusAmount(Long amount) {
+        this.amount += amount;
+    }
+}
