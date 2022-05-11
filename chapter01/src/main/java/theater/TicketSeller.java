c4c72165b4441923aed0bdfea0b2bce727e8143e
YONGJU <odetsuo@gmail.com>
2022-05-12 08:16:46 +0900

    refactor: 매표소 책임 할당
    판매원이 매표소의 티켓을 마음대로 빼가서 관람객에게 주고, 판매 금액을 넣고 있다.
    매표소가 직접 관람객에게 표를 주고 판매금액을 증가 시키는 책임을 가지게 한다.

--- chapter01/src/main/java/theater/TicketSeller.java
+++ chapter01/src/main/java/theater/TicketSeller.java
@@ -9,9 +9,7 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        Ticket ticket = ticketOffice.getTicket();
-        Long fee = audience.buy(ticket);
-        ticketOffice.plusAmount(fee);
+        ticketOffice.sellTicketTo(audience);
     }
 
 }
