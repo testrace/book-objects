75546df0cacd5e426b75b5dde6932b0a486ff2a0
YONGJU <odetsuo@gmail.com>
2022-05-13 08:26:38 +0900

    feat: 영화 예매

---
+++ chapter02/src/main/java/movie/Movie.java
@@ -0,0 +1,12 @@
+package movie;
+
+public class Movie {
+
+    public Money getFee() {
+        return Money.wons(0L);
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return null;
+    }
+}
---
+++ chapter02/src/main/java/movie/Reservation.java
@@ -0,0 +1,16 @@
+package movie;
+
+public class Reservation {
+
+    private Customer customer;
+    private Screening screening;
+    private Money fee;
+    private int audienceCount;
+
+    public Reservation(Customer customer, Screening screening, Money calculateFee, int audienceCount) {
+        this.customer = customer;
+        this.screening = screening;
+        this.fee = calculateFee;
+        this.audienceCount = audienceCount;
+    }
+}
