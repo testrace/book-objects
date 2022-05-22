70ccdd7e5d897071705caf00f9daa352c856c65b
YONGJU <odetsuo@gmail.com>
2022-05-23 08:11:27 +0900

    feat: 예약

---
+++ chapter04/src/main/java/movie/Reservation.java
@@ -0,0 +1,33 @@
+package movie;
+
+public class Reservation {
+
+    private Customer customer;
+    private Screening screening;
+    private Money fee;
+    private int audienceCount;
+
+    public Reservation(Customer customer, Screening screening, Money fee, int audienceCount) {
+        this.customer = customer;
+        this.screening = screening;
+        this.fee = fee;
+        this.audienceCount = audienceCount;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Screening getScreening() {
+        return screening;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public int getAudienceCount() {
+        return audienceCount;
+    }
+
+}
