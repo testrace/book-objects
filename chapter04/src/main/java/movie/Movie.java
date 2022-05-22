a7ab0e52b14f6eb69641944ad50914d2bef101d8
YONGJU <odetsuo@gmail.com>
2022-05-23 08:05:44 +0900

    feat: Movie의 내부 데이터를 반환하고, 변경하는 메서드 추가

--- chapter04/src/main/java/movie/Movie.java
+++ chapter04/src/main/java/movie/Movie.java
@@ -1,5 +1,7 @@
 package movie;
 
+import java.util.Collections;
+
 public class Movie {
 
     private String title;
@@ -11,4 +13,43 @@ public class Movie {
     private Money discountAmount;
     private double discountPercent;
 
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFee(Money fee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return Collections.unmodifiableList(discountConditions);
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
 }
