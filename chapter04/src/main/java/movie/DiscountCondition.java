2e5848ce2a510a10dd4fcea841f3930d0e7b508b
YONGJU <odetsuo@gmail.com>
2022-05-24 08:08:52 +0900

    refactor: 할인 가능 여부 로직을 DiscountCondition 으로 이동

--- chapter04/src/main/java/movie/DiscountCondition.java
+++ chapter04/src/main/java/movie/DiscountCondition.java
@@ -17,6 +17,24 @@ public class DiscountCondition {
         return type;
     }
 
+    public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
+        if (type != DiscountConditionType.PERIOD) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.dayOfWeek.equals(dayOfWeek) &&
+            this.startTime.compareTo(time) <= 0 &&
+            this.endTime.compareTo(time) >= 0;
+    }
+
+    public boolean isDiscountable(int sequence) {
+        if (type != DiscountConditionType.SEQUENCE) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.sequence == sequence;
+    }
+
     public void setType(DiscountConditionType type) {
         this.type = type;
     }
