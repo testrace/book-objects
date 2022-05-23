9fa84c68e70110e3f50abe935065d35763679556
YONGJU <odetsuo@gmail.com>
2022-05-24 08:04:18 +0900

    refactor: 사각형의 크기를 조절하는 메서드를 제공
    사각형의 상태를 캡슐화하여 사각형의 크기를 조절하는 인터페이스를 제공함
    사각형의 크기를 Rectangle 스스로 증가시키도록 책임을 이동함

--- chapter04/src/main/java/rectangle/Rectangle.java
+++ chapter04/src/main/java/rectangle/Rectangle.java
@@ -14,35 +14,9 @@ public class Rectangle {
         this.bottom = bottom;
     }
 
-    public int getLeft() {
-        return left;
+    public void enlarge(int multiple) {
+        this.right = this.right * multiple;
+        this.bottom = this.bottom * multiple;
     }
 
-    public void setLeft(int left) {
-        this.left = left;
-    }
-
-    public int getTop() {
-        return top;
-    }
-
-    public void setTop(int top) {
-        this.top = top;
-    }
-
-    public int getRight() {
-        return right;
-    }
-
-    public void setRight(int right) {
-        this.right = right;
-    }
-
-    public int getBottom() {
-        return bottom;
-    }
-
-    public void setBottom(int bottom) {
-        this.bottom = bottom;
-    }
 }
--- chapter04/src/main/java/rectangle/RectangleService.java
+++ chapter04/src/main/java/rectangle/RectangleService.java
@@ -3,8 +3,7 @@ package rectangle;
 public class RectangleService {
 
     void changeSize(Rectangle rectangle, int multiple) {
-        rectangle.setRight(rectangle.getRight() * multiple);
-        rectangle.setBottom(rectangle.getBottom() * multiple);
+        rectangle.enlarge(multiple);
     }
 
 }
