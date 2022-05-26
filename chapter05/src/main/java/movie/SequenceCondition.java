dde9a2401a8c9d052fff6c68782fd78f6a124df7
YONGJU <odetsuo@gmail.com>
2022-05-27 08:17:03 +0900

    refactor: DiscountCondition 응집도 높이기 위해 클래스 분리

---
+++ chapter05/src/main/java/movie/SequenceCondition.java
@@ -0,0 +1,14 @@
+package movie;
+
+public class SequenceCondition {
+
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean isSatisfiedBy(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+}
