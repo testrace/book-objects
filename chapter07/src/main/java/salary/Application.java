acc7c760eff45890f1ca1fe39a4df095e1bb86ee
YONGJU <odetsuo@gmail.com>
2022-06-08 07:55:41 +0900

    feat: 급여 관리

--- chapter07/src/main/java/salary/Application.java
+++ chapter07/src/main/java/salary/Application.java
@@ -11,8 +11,7 @@ public class Application {
     private static int[] basePays = {400, 300, 250};
 
     public static void main(String[] args) {
-
-        String name = args[0];
+        String name = "직원C";
 
         double taxRate = getTaxRate();
         int pay = calculatePayFor(name, taxRate);
@@ -20,22 +19,22 @@ public class Application {
     }
 
     private static double getTaxRate() {
-        System.out.println("세율을 입력하세요:");
+        System.out.print("세율을 입력하세요: ");
         return Double.parseDouble(SCANNER.nextLine());
     }
 
     private static int calculatePayFor(String name, double taxRate) {
         int index = Arrays.asList(EMPLOYEES).indexOf(name);
         int basePay = basePays[index];
-        return basePay - (basePay * (int)taxRate);
+        return (int)(basePay - (basePay * taxRate));
     }
 
     private static String describeResult(String name, long pay) {
-        return null;
+        return String.format("이름: %s, 급여: %d", name, pay);
     }
 
     private static void puts(String describeResult) {
-
+        System.out.println(describeResult);
     }
 
 }
