f0879d40782540e0b0710fed42ad2249f9934ce6
YONGJU <odetsuo@gmail.com>
2022-06-10 08:08:04 +0900

    refactor: Employees 모듈화
    Employees를 모듈화하여 메인 함수에서 모듈 내부를 알지 못하게 한다

--- chapter07/src/main/java/salary/Application.java
+++ chapter07/src/main/java/salary/Application.java
@@ -1,16 +1,10 @@
 package salary;
 
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Application {
 
     private static final Scanner SCANNER = new Scanner(System.in);
-    private static final String[] EMPLOYEES = {"직원A", "직원B", "직원C", "아르바이트D", "아르바이트E", "아르바이트F"};
-    private static final double[] BASE_PAYS = {400, 300, 250, 1, 1, 1.5};
-    private static final boolean[] HOURLYS = {false, false, false, true, true, true};
-    private static final int[] TIME_CARDS = {0, 0, 0, 120, 120, 120};
 
     public static void main(String[] args) {
         String operation = args[0];
@@ -23,44 +17,25 @@ public class Application {
 
     }
 
+    private static void sumOfBasePays() {
+        puts("급여 총액: " + Employees.sumOfBasePays());
+    }
+
     private static void calculatePay(String name) {
         double taxRate = getTaxRate();
         double pay;
-        if (hourly(name)) {
-            pay = calculateHourlyPayFor(name, taxRate);
+        if (Employees.hourly(name)) {
+            pay = Employees.calculateHourlyPayFor(name, taxRate);
         } else {
-            pay = calculatePayFor(name, taxRate);
+            pay = Employees.calculatePayFor(name, taxRate);
         }
         puts(describeResult(name, pay));
     }
 
-    private static boolean hourly(String name) {
-        return HOURLYS[Arrays.asList(EMPLOYEES).indexOf(name)];
-    }
-
-    private static void sumOfBasePays() {
-        double result = IntStream.range(0, EMPLOYEES.length)
-            .filter(i -> !hourly(EMPLOYEES[i]))
-            .mapToDouble(i -> BASE_PAYS[i])
-            .sum();
-        puts("급여 총액: " + result);
-    }
 
     private static double getTaxRate() {
         System.out.print("세율을 입력하세요: ");
         return Double.parseDouble(SCANNER.nextLine());
-    }
-
-    private static double calculatePayFor(String name, double taxRate) {
-        int index = Arrays.asList(EMPLOYEES).indexOf(name);
-        double basePay = BASE_PAYS[index];
-        return (basePay - (basePay * taxRate));
-    }
-
-    private static double calculateHourlyPayFor(String name, double taxRate) {
-        int index = Arrays.asList(EMPLOYEES).indexOf(name);
-        double basePay = BASE_PAYS[index] * TIME_CARDS[index];
-        return (basePay - (basePay * taxRate));
     }
 
     private static String describeResult(String name, double pay) {
---
+++ chapter07/src/main/java/salary/Employees.java
@@ -0,0 +1,38 @@
+package salary;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Employees {
+
+    private static final String[] EMPLOYEES = {"직원A", "직원B", "직원C", "아르바이트D", "아르바이트E", "아르바이트F"};
+    private static final double[] BASE_PAYS = {400, 300, 250, 1, 1, 1.5};
+    private static final boolean[] HOURLYS = {false, false, false, true, true, true};
+    private static final int[] TIME_CARDS = {0, 0, 0, 120, 120, 120};
+
+
+
+    public static boolean hourly(String name) {
+        return HOURLYS[Arrays.asList(EMPLOYEES).indexOf(name)];
+    }
+
+    public static double sumOfBasePays() {
+        return IntStream.range(0, EMPLOYEES.length)
+            .filter(i -> !hourly(EMPLOYEES[i]))
+            .mapToDouble(i -> BASE_PAYS[i])
+            .sum();
+
+    }
+
+    public static double calculatePayFor(String name, double taxRate) {
+        int index = Arrays.asList(EMPLOYEES).indexOf(name);
+        double basePay = BASE_PAYS[index];
+        return (basePay - (basePay * taxRate));
+    }
+
+    public static double calculateHourlyPayFor(String name, double taxRate) {
+        int index = Arrays.asList(EMPLOYEES).indexOf(name);
+        double basePay = BASE_PAYS[index] * TIME_CARDS[index];
+        return (basePay - (basePay * taxRate));
+    }
+}
