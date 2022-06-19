2aeb101cbe00083c8f9e74b7e244d105d64afa3f
YONGJU <odetsuo@gmail.com>
2022-06-20 08:23:31 +0900

    feat: 타입 코드를 활용한 중복 제거

--- chapter10/src/main/java/call/Phone.java
+++ chapter10/src/main/java/call/Phone.java
@@ -6,16 +6,32 @@ import java.util.List;
 
 public class Phone {
 
+    public static final int LATE_NIGHT_HOUR = 22;
+    enum PhoneType { REGULAR, NIGHTLY }
+
+    private PhoneType phoneType;
+
     private Money amount;
+    private Money nightlyAmount;
+    private Money regularAmount;
     private Duration seconds;
     private List<Call> calls = new ArrayList<>();
     private double taxRate;
 
+    public Phone(Money amount, Duration seconds) {
+        this(PhoneType.REGULAR, amount, Money.ZERO, Money.ZERO, seconds);
+    }
 
-    public Phone(Money amount, Duration seconds, double taxRate) {
+    public Phone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this(PhoneType.NIGHTLY, Money.ZERO, nightlyAmount, regularAmount, seconds);
+    }
+
+    public Phone(PhoneType type, Money amount, Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this.phoneType = type;
         this.amount = amount;
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
         this.seconds = seconds;
-        this.taxRate = taxRate;
     }
 
     public void call(Call call) {
@@ -38,9 +54,17 @@ public class Phone {
         Money result = Money.ZERO;
 
         for (Call call : calls) {
-            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            if (phoneType == PhoneType.REGULAR) {
+                result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            } else {
+                if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
+                    result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+                } else {
+                    result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+                }
+            }
         }
 
-        return result.plus(result.times(taxRate));
+        return result;
     }
 }
