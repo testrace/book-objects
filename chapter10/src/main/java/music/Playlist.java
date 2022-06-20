00026cc656c984f9632a44a2e675183c9b66363d
YONGJU <odetsuo@gmail.com>
2022-06-21 07:41:34 +0900

    feat: 플레이리스트

---
+++ chapter10/src/main/java/music/Playlist.java
@@ -0,0 +1,17 @@
+package music;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Playlist {
+
+    private List<Song> tracks = new ArrayList<>();
+
+    public void append(Song song) {
+        getTracks().add(song);
+    }
+
+    public List<Song> getTracks() {
+        return tracks;
+    }
+}
