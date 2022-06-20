c2b0312e0f32b7b8d9bc5dd2931605cb02d20397
YONGJU <odetsuo@gmail.com>
2022-06-21 07:40:35 +0900

    feat: 음악

---
+++ chapter10/src/main/java/music/Song.java
@@ -0,0 +1,20 @@
+package music;
+
+public class Song {
+
+    private String singer;
+    private String title;
+
+    public Song(String singer, String title) {
+        this.singer = singer;
+        this.title = title;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
