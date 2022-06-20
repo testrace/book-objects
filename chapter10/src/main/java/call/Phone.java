1e4a5416082e10218b3e0d905babb1312b1bb11b
YONGJU <odetsuo@gmail.com>
2022-06-21 08:14:05 +0900

    refactor: 요금계산 메서드 추출

--- chapter10/src/main/java/call/Phone.java
+++ chapter10/src/main/java/call/Phone.java
@@ -6,31 +6,12 @@ import java.util.List;
 
 public class Phone {
 
-    public static final int LATE_NIGHT_HOUR = 22;
-    enum PhoneType { REGULAR, NIGHTLY }
-
-    private PhoneType phoneType;
-
     private Money amount;
-    private Money nightlyAmount;
-    private Money regularAmount;
     private Duration seconds;
     private List<Call> calls = new ArrayList<>();
-    private double taxRate;
 
     public Phone(Money amount, Duration seconds) {
-        this(PhoneType.REGULAR, amount, Money.ZERO, Money.ZERO, seconds);
-    }
-
-    public Phone(Money nightlyAmount, Money regularAmount, Duration seconds) {
-        this(PhoneType.NIGHTLY, Money.ZERO, nightlyAmount, regularAmount, seconds);
-    }
-
-    public Phone(PhoneType type, Money amount, Money nightlyAmount, Money regularAmount, Duration seconds) {
-        this.phoneType = type;
         this.amount = amount;
-        this.nightlyAmount = nightlyAmount;
-        this.regularAmount = regularAmount;
         this.seconds = seconds;
     }
 
@@ -54,17 +35,13 @@ public class Phone {
         Money result = Money.ZERO;
 
         for (Call call : calls) {
-            if (phoneType == PhoneType.REGULAR) {
-                result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-            } else {
-                if (call.getFrom().getHour() >= LATE_NIGHT_HOUR) {
-                    result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-                } else {
-                    result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
-                }
-            }
+            result = result.plus(calculateCallFee(call));
         }
 
         return result;
+    }
+
+    private Money calculateCallFee(Call call) {
+        return amount.times(call.getDuration().getSeconds() / seconds.getSeconds());
     }
 }
