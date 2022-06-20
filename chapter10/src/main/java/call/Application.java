17b1e7184fa3ef8c6bafeb7ad087e30de07e27e2
YONGJU <odetsuo@gmail.com>
2022-06-21 08:28:12 +0900

    feat: 전화요금 세금 추가

--- chapter10/src/main/java/call/Application.java
+++ chapter10/src/main/java/call/Application.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 public class Application {
 
     public static void main(String[] args) {
-        Phone phone = new RegularPhone(Money.wons(5), Duration.ofSeconds(10));
+        Phone phone = new RegularPhone(Money.wons(5), Duration.ofSeconds(10), 0.1);
 
         phone.call(new Call(LocalDateTime.of(2018, 1, 1, 12, 10, 0),
             LocalDateTime.of(2018, 1, 1, 12, 11, 0)));
--- chapter10/src/main/java/call/Phone.java
+++ chapter10/src/main/java/call/Phone.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 public abstract class Phone {
 
+ private double taxRate;
     private List<Call> calls = new ArrayList<>();
+
+    public Phone(double taxRate) {
+        this.taxRate = taxRate;
+    }
 
     public void call(Call call) {
         calls.add(call);
@@ -18,7 +23,7 @@ public abstract class Phone {
             result = result.plus(calculateCallFee(call));
         }
 
-        return result;
+        return result.plus(result.times(taxRate));
     }
 
     protected abstract Money calculateCallFee(Call call);
