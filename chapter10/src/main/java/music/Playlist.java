eb3b031c8026e747aef3491ece4fe21d5c869944
YONGJU <odetsuo@gmail.com>
2022-06-21 07:43:59 +0900

    feat: 가수별 노래 목록

--- chapter10/src/main/java/music/Playlist.java
+++ chapter10/src/main/java/music/Playlist.java
@@ -1,17 +1,25 @@
 package music;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Playlist {
 
     private List<Song> tracks = new ArrayList<>();
+    private Map<String, String> singers = new HashMap<>();
 
     public void append(Song song) {
         getTracks().add(song);
+        singers.put(song.getSinger(), song.getSinger());
     }
 
     public List<Song> getTracks() {
         return tracks;
+    }
+
+    public Map<String, String> getSingers() {
+        return singers;
     }
 }
