4bb86bceeb680654d015e489fdb59f465077af76
YONGJU <odetsuo@gmail.com>
2022-06-22 08:43:10 +0900

    feat: 요금 할인 정책

---
+++ chapter11/src/main/java/call/RateDiscountablePolicy.java
@@ -0,0 +1,16 @@
+package call;
+
+public class RateDiscountablePolicy extends AdditionalRatePolicy {
+
+    private Money discountAmount;
+
+    public RateDiscountablePolicy(RatePolicy next, Money discountAmount) {
+        super(next);
+        this.discountAmount = discountAmount;
+    }
+
+    @Override
+    protected Money afterCalculated(Money fee) {
+        return fee.minus(discountAmount);
+    }
+}
