8cd487e9c167d192e510ae51c1126ecd84e5c17b
YONGJU <odetsuo@gmail.com>
2022-06-30 07:57:41 +0900

    feat: 성적 등급 평균

--- chapter12/src/main/java/lecture/GradeLecture.java
+++ chapter12/src/main/java/lecture/GradeLecture.java
@@ -17,7 +17,23 @@ public class GradeLecture extends Lecture {
         return super.evaluate() + ", " + gradesStatistics();
     }
 
-    public String gradesStatistics() {
+    public double average(String gradeName) {
+        return grades.stream()
+            .filter(grade -> grade.isName(gradeName))
+            .findFirst()
+            .map(this::gradeAverage)
+            .orElse(0.0);
+    }
+
+    private double gradeAverage(Grade grade) {
+        return getScores().stream()
+            .filter(grade::include)
+            .mapToInt(Integer::intValue)
+            .average()
+            .orElse(0);
+    }
+
+    private String gradesStatistics() {
         return grades.stream()
             .map(this::format)
             .collect(Collectors.joining(" "));
