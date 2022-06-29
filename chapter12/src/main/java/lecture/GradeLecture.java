f942ca97cc0091183da9d4d8bd3f1f79f9665008
YONGJU <odetsuo@gmail.com>
2022-06-30 07:55:11 +0900

    feat: 성적 등급

---
+++ chapter12/src/main/java/lecture/GradeLecture.java
@@ -0,0 +1,35 @@
+package lecture;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class GradeLecture extends Lecture {
+
+    private List<Grade> grades;
+
+    public GradeLecture(int pass, String title, List<Integer> scores, List<Grade> grades) {
+        super(pass, title, scores);
+        this.grades = grades;
+    }
+
+    @Override
+    public String evaluate() {
+        return super.evaluate() + ", " + gradesStatistics();
+    }
+
+    public String gradesStatistics() {
+        return grades.stream()
+            .map(this::format)
+            .collect(Collectors.joining(" "));
+    }
+
+    private String format(Grade grade) {
+        return String.format("%s:%d", grade.getName(), gradeCount(grade));
+    }
+
+    private long gradeCount(Grade grade) {
+        return getScores().stream()
+            .filter(grade::include)
+            .count();
+    }
+}
