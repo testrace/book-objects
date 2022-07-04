be2481ab8e8c695dad6a9a4762837a2033f8c970
YONGJU <odetsuo@gmail.com>
2022-07-05 08:26:24 +0900

    feat: 고전적인 리스코프 치환 원칙의 잘못된 예시(정사각형 is a 직사각형)

---
+++ chapter13/src/main/java/badliskovsubstitutionprinciple/Square.java
@@ -0,0 +1,21 @@
+package badliskovsubstitutionprinciple;
+
+public class Square extends Rectangle {
+
+
+    public Square(int x, int y, int size) {
+        super(x, y, size, size);
+    }
+
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+        super.setWidth(height);
+        super.setHeight(height);
+    }
+}
