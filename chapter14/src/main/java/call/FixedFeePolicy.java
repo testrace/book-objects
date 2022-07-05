c7ed9ca6c4777af6be639f2b7a50b5ff28b25c28
YONGJU <odetsuo@gmail.com>
2022-07-06 08:36:51 +0900

    refactor: 고정 요금 정책

--- chapter14/src/main/java/call/FixedFeePolicy.java
+++ chapter14/src/main/java/call/FixedFeePolicy.java
@@ -2,12 +2,12 @@ package call;
 
 import java.time.Duration;
 
-public class RegularPolicy extends BasicRatePolicy {
+public class FixedFeePolicy extends BasicRatePolicy {
 
     private Money amount;
     private Duration seconds;
 
-    public RegularPolicy(Money amount, Duration seconds) {
+    public FixedFeePolicy(Money amount, Duration seconds) {
         this.amount = amount;
         this.seconds = seconds;
     }
