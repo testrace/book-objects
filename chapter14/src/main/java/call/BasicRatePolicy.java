03cbbf8975436ce517453f7ff001f09dbf1fe605
YONGJU <odetsuo@gmail.com>
2022-07-16 11:18:58 +0900

    refactor: 요금 계산 정책 변경

--- chapter14/src/main/java/call/BasicRatePolicy.java
+++ chapter14/src/main/java/call/BasicRatePolicy.java
@@ -1,19 +1,30 @@
 package call;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class BasicRatePolicy implements RatePolicy {
+
+    private List<FeeRule> feeRules = new ArrayList<>();
+
+    public BasicRatePolicy(FeeRule... feeRules) {
+        this.feeRules = Arrays.asList(feeRules);
+    }
 
     @Override
     public Money calculateFee(Phone phone) {
-        Money result = Money.ZERO;
+        return phone.getCalls().stream()
+            .map(this::calculate)
+            .reduce(Money.ZERO, Money::plus);
+    }
 
-        for (Call call : phone.getCalls()) {
-            result.plus(calculateCallFee(call));
-        }
-
-        return result;
+    private Money calculate(Call call) {
+        return feeRules.stream()
+            .map(rule -> rule.calculateFee(call))
+            .reduce(Money.ZERO, Money::plus);
     }
 
     protected abstract Money calculateCallFee(Call call);
-
 
 }
